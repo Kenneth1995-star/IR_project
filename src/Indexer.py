87fb4921ceea67b1930052e91747ec25f888fd76
KinNingShum <kinningshum@hotmail.com>
2025-10-28 04:14:29 +0100

    Partial SPIMI implementation

--- src/Indexer.py
+++ src/Indexer.py
@@ -1,7 +1,11 @@
 from tokenizer import Tokenizer
 from DocumentManager import DocumentManager
+from Vocabulary import Vocabulary
 from collections import defaultdict
 from typing import Optional
+from functools import partial
+import pickle
+import gzip
 
 
 class Indexer:
@@ -10,21 +14,56 @@ class Indexer:
         self.manager = DocumentManager()
         self.posting_list = defaultdict(list)
 
-    def build_index(self, filestream):
+    def build_index_in_memory(self, filestream):
+        """
+        In-memory posting list building with term frequency and positions
+        """
         while filepath := next(filestream, None):
             doc_id, content = self.manager.read_document(filepath)
 
-            temp = defaultdict(lambda: {"frequency": 0, "positions": []})
+            temp = defaultdict(list)
             for position, token in enumerate(self.tokenizer.tokenize(content)):
-                temp[token]["frequency"] += 1
-                temp[token]["positions"].append(position)
+                temp[token].append(position)
 
             for token, data in temp.items():
                 self.posting_list[token].append({
                     "doc_id": doc_id,
-                    "frequency": data["frequency"],
-                    "positions": data["positions"]
+                    "frequency": len(data),
+                    "positions": data
                 })
+
+
+    def build_index(self, filestream, posting_limit=1_000_000):
+        """
+        SPIMI implementation
+        """
+        block_count = 0
+        posting_count = 0
+
+        dictionary: dict[int, dict[int, list[int]]] = defaultdict(partial(defaultdict, list))
+        vocabulary: Vocabulary = Vocabulary()
+
+        while filepath := next(filestream, None):
+            doc_id, content = self.manager.read_document(filepath)
+
+            for position, token in enumerate(self.tokenizer.tokenize(content)):
+                term_id = vocabulary.get_id(token)
+                posting_list = dictionary[term_id]
+                posting_list[doc_id].append(position)
+                posting_count += 1
+
+                if posting_count >= posting_limit:
+                    self._writeblock(dictionary, vocabulary)
+                    block_count += 1
+                    posting_count = 0
+                    dictionary = defaultdict(partial(defaultdict, list))
+                    vocabulary = Vocabulary()
+        
+        self._writeblock(dictionary, vocabulary)
+
+    def _writeblock(self, dictinary, vocabulary: Vocabulary):
+        pass
+
 
 
 def filestream():
